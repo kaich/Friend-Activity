bffbe3138587b7f040191fd4e306882250c3c001
mac <mac@macdeMac-mini-2.local>
2015-08-05 21:01:45 +0800

    tody alter

--- FriendParty/app/controllers/groups_controller.rb
+++ FriendParty/app/controllers/groups_controller.rb
@@ -9,11 +9,12 @@ class GroupsController < ApplicationController
     @group = current_user.groups.build(group_params)
     
     respond_to do |wants|
-      if @group.save
+      if current_user.save
         flash[:notice] = ' was successfully created.'
         wants.html { redirect_to(@group) }
         wants.xml { render :xml => @group, :status => :created, :location => @group }
       else
+        flash[:notice] = "unsuccessfully created..."
         wants.html { render :action => "new" }
         wants.xml { render :xml => @group.errors, :status => :unprocessable_entity }
       end
@@ -23,12 +24,26 @@ class GroupsController < ApplicationController
   def index 
     current_user.groups
   end
+
+  def show
+    @group = Group.find(params[:id])
+    @users = @group.users.paginate(:page => params[:page],:per_page => 10)
+  
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render xml: @group }
+    end
+  end
+
+  def applyJoinGroup 
+    
+  end
   
 
   private 
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def group_params
-    params.require(:todo).permit(:name, :intro)
+    params.require(:group).permit(:name, :intro)
   end
 end
--- FriendParty/app/models/group.rb
+++ FriendParty/app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
    validates :name , length:{maximum:50} , presence: true
-   validates :intro , length:{maximum:200}, allow_nil: true
+   validates :intro , length:{maximum:1000}, allow_nil: true
    has_and_belongs_to_many :users
 end
